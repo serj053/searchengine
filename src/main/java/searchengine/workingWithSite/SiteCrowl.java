53f4af9a263d3fae04f6304d5cd604e5ece42b2a
Serj <serj053@yandex.ru>
2024-01-19 17:01:22 +0300

    добавлена зависимость в pom.xml и класс SteCrowl{} для скачивая контента с сайта

--- src/main/java/searchengine/workingWithSite/SiteCrowl.java
+++ src/main/java/searchengine/workingWithSite/SiteCrowl.java
@@ -1,5 +1,27 @@
 package searchengine.workingWithSite;
 
-public class SiteCrowl {
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+import java.io.IOException;
+
+public class SiteCrowl {
+    public static String[] connect(String urlSite) throws IOException {
+        String[] list = new String[2];
+        Document doc = Jsoup.connect(urlSite).ignoreHttpErrors(true).get();
+        Elements elements = doc.select("a");
+        int n = 0;
+        for (Element element : elements) {
+            if (n == 2 && list[0] != null) {
+                String url = element.attr("abs:href");
+                Document doc2 = Jsoup.connect(url).ignoreHttpErrors(true).get();
+                list[0] = url;
+                list[1] = doc2.text();
+                n++;
+            }
+        }
+        return list;
+    }
 }
