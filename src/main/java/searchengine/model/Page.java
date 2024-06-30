3d4f9e644c9855ad4914aca6f0bbcb08d3799a9d
serj053 <serj053@yandex.ru>
2024-06-30 19:54:40 +0300

    update

--- src/main/java/searchengine/model/Page.java
+++ src/main/java/searchengine/model/Page.java
@@ -9,14 +9,16 @@ public class Page {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     // @Column(name="site_id", nullable = false)
-    @ManyToOne(cascade = CascadeType.ALL )
+    @ManyToOne(cascade = CascadeType.ALL)
     //@JoinColumn(name= "id" )
     private SiteDB site;
     @Column(nullable = false)
     private String path;
     @Column(nullable = false)
     private int code;
-    @Column(nullable = false)
+    //@Column(nullable = false, length=20000)
+    @Column(columnDefinition = "TEXT",
+            name = "page_content")
     private String content;
 
     public Page(SiteDB site, String path, int code, String content) {
